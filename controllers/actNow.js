2507f098011f8e4788de63fa4bf3471abc28090d
justinluu8235 <justinluu8235@gmail.com>
2022-01-04 14:15:19 -0800

    added site and actNow controller

---
+++ controllers/actNow.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+
+// - Fetch API to display top 10 states for COVID: display state name and number of COVID cases 
+
+// - Fetch API data by county name. Display # of cases, Fully vaccinated %, # of deaths, new cases
+
+
+
+module.exports = router;
---
+++ controllers/site.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const {Site} = require('../models')
+
+// - Display all sites at zip 
+
+// - Individual site page - display all the info including comments 
+
+// - New site route 
+
+
+module.exports = router;
