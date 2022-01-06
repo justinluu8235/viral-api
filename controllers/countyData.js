3ee3f2762f7d99153bcdf24cd1fdc644c05ce82a
chenmichael2 <chenmichael2012@gmail.com>
2022-01-06 11:42:50 -0800

    edited apiTest.js and site.js, provides a route of location data based on zip code

---
+++ controllers/countyData.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const {CountyData} = require('../models')
+
+
+
+
+
+
+// - return all the counties
+router.get("/counties" , async (request, response) => {
+    try{
+        let countyNameArr = []
+        let countyDataArr = await CountyData.find();
+        for(let i=0; i<countyDataArr.length; i++){
+            let countyData = countyDataArr[i];
+            let name = countyData.countyName;
+            countyNameArr.push(name);
+        }
+
+
+        response.json({countyNameArr});
+    }
+    catch(error){
+        response.status(500).send(error);
+    }
+});
+
+module.exports = router;
